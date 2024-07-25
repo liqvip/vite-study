d60df208fe682cbdbd9dde1026f5082028741d50
liqiang <liqiang@makeblock.com>
2024-07-25 11:16:53 +0800

    feat: vite 环境变量配置

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import viteBaseConfig from "./vite.base.config";
 import viteDevConfig from "./vite.dev.config";
 import viteProdConfig from "./vite.prod.config";
@@ -15,7 +15,9 @@ const envResolver = {
 };
 
 export default defineConfig((env) => {
-    // 是 build 还是 server 主要取决于我们敲的命令是开启开发环境还是生产环境
-    console.log('command', env.command);
+    // 是 build 还是 serve 主要取决于我们敲的命令是开启开发环境还是生产环境
+    console.log('env', env);
+    const envConfig = loadEnv(env.mode, process.cwd());
+    console.log('envConfig', envConfig);
     return envResolver[env.command]();
 });
